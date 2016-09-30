6b223dfc4c846e3cff52025356c2cd70c545cb27
Marco Orovecchia <marco@orovecchia.at>
2016-09-30 12:13:25 +0200

    initial commit for health check

--- app.go
+++ app.go
@@ -1,13 +1,42 @@
+/*
+Copyright 2014 The Kubernetes Authors All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// A simple server that is alive for 10 seconds, then reports unhealthy for
+// the rest of its (hopefully) short existence.
 package main
 
 import (
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/", fs)
-  log.Println("Listening on 8080...")
-  http.ListenAndServe(":8080", nil)
+	started := time.Now()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		duration := time.Now().Sub(started)
+		if duration.Seconds() > 10 {
+			w.WriteHeader(500)
+			w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+		} else {
+			w.WriteHeader(200)
+			w.Write([]byte("ok"))
+		}
+
+	})
+	log.Println(http.ListenAndServe(":8080", nil))
 }
